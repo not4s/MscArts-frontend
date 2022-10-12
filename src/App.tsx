d08410a582ba6a7241196337487d944999937f91
Matt Neave <matthewneaveis@hotmail.co.uk>
2022-10-12 17:59:26 +0100

    Revert "Added antd dependancy and navbar"
    This reverts commit a483201a8d60bb23040f2ae97a7d184842257e66.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import SpreadsheetUpload from "./components/SpreadsheetUpload";
-import VisulisationNagivation from "./components/VisulisationNagivation";
 
 export default function App() {
   return (
     <div className="App">
-      <VisulisationNagivation />
       <SpreadsheetUpload />
     </div>
   );
