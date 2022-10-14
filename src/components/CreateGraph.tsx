badb5206b22c89579f05568fada50cd5a2db1af2
Matt Neave <matthewneaveis@hotmail.co.uk>
2022-10-14 22:31:13 +0100

    Experimental form

--- src/components/CreateGraph.tsx
+++ src/components/CreateGraph.tsx
@@ -4,21 +4,31 @@ import {
   PieChartOutlined,
   TableOutlined,
   PlusOutlined,
+  ExclamationCircleOutlined,
 } from "@ant-design/icons";
-import { Button, Modal, Select } from "antd";
+import { Button, Form, Modal, Select, Tag } from "antd";
 import type { MenuProps } from "antd";
+import FormItem from "antd/es/form/FormItem";
 
 const { Option } = Select;
 
 const CreateGraph = () => {
   const [isModalOpen, setModalOpen] = useState(false);
+  const [graphType, setGraphType] = useState("");
+  const [form] = Form.useForm();
 
   const handleOk = () => {
-    setModalOpen(false);
+    form.submit();
+    console.log(Object.values(form.getFieldsValue()));
+    if (!Object.values(form.getFieldsValue()).includes(undefined)) {
+      setModalOpen(false);
+    }
   };
 
-  const handleChange = (value: string) => {
-    console.log(`selected ${value}`);
+  const handleCancel = () => {
+    setModalOpen(false);
+    form.resetFields();
+    setGraphType("");
   };
 
   return (
@@ -27,23 +37,72 @@ const CreateGraph = () => {
         title="Create a new visulisation"
         open={isModalOpen}
         onOk={handleOk}
-        onCancel={() => setModalOpen(false)}
+        okText="Submit"
+        onCancel={handleCancel}
       >
-        <Select
-          placeholder="Select visulisation type"
-          style={{ width: 240 }}
-          onChange={handleChange}
-        >
-          <Option value="BAR">
-            <BarChartOutlined /> Bar Chart
-          </Option>
-          <Option value="PIE">
-            <PieChartOutlined /> Pie Chart
-          </Option>
-          <Option value="TABLE">
-            <TableOutlined /> Table{" "}
-          </Option>
-        </Select>
+        <Form form={form}>
+          <Form.Item name="Visulisation" rules={[{ required: true }]}>
+            <Select
+              placeholder="Select visulisation type"
+              style={{ width: 240 }}
+              onChange={(value: string) => setGraphType(value)}
+            >
+              <Option value="BAR">
+                <BarChartOutlined /> Bar Chart
+              </Option>
+              <Option value="PIE">
+                <PieChartOutlined /> Pie Chart
+              </Option>
+              <Option value="TABLE">
+                <TableOutlined /> Table{" "}
+              </Option>
+            </Select>
+          </Form.Item>
+
+          {graphType === "BAR" ? (
+            <>
+              {/* ---------------------------------------- */}
+
+              <Form.Item
+                name="Columns"
+                rules={[{ required: true }]}
+                extra="E.g. 'Gender' will create columns for 'Male' and 'Female' respectfully"
+              >
+                <Select
+                  placeholder="Select columns"
+                  style={{ width: 240 }}
+                  // onChange={(value: string) => setGraphType(value)}
+                >
+                  <Option value="GENDER">Gender</Option>
+                  <Option value="COURSE">Course</Option>
+                  <Option value="FEE_STATUS">Fee Status</Option>
+                </Select>
+              </Form.Item>
+
+              {/* ---------------------------------------- */}
+
+              <Form.Item
+                name="Grouping"
+                rules={[{ required: false }]}
+                extra="E.g. 'Gender' will display the difference between 'Male' and 'Female' within a single column"
+              >
+                <Select
+                  placeholder="Select grouping (optional)"
+                  style={{ width: 240 }}
+                  // onChange={(value: string) => setGraphType(value)}
+                >
+                  <Option value="GENDER">Gender</Option>
+                  <Option value="COURSE">Course</Option>
+                  <Option value="FEE_STATUS">Fee Status</Option>
+                </Select>
+              </Form.Item>
+
+              {/* ---------------------------------------- */}
+            </>
+          ) : (
+            <></>
+          )}
+        </Form>
       </Modal>
       <Button type="dashed" onClick={() => setModalOpen(true)}>
         <PlusOutlined />
