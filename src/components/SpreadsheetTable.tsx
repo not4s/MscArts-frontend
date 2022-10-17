6714213e422677d0d2f6b7266de54694bc66d088
tahmid148 <tahmidrahman2812@outlook.com>
2022-10-17 11:59:29 +0100

    feat: created a table that stores names and dates; also added dummy data

--- src/components/SpreadsheetTable.tsx
+++ src/components/SpreadsheetTable.tsx
@@ -1,14 +1,53 @@
-import React, { useState } from "react";
-import { Space, Table, Tag } from "antd";
-import type { ColumnsType } from "antd/es/table";
+import { Table } from "antd";
+import { ColumnsType, TableProps } from "antd/lib/table";
+import moment from "moment";
+import React from "react";
 
 export const SpreadsheetTable = () => {
-  interface dataType {
-    key: string;
+  interface DataType {
+    key: React.Key;
     name: string;
-    date: Date;
-    tags: string[];
+    date: string;
   }
 
-  return <div></div>;
+  const columns: ColumnsType<DataType> = [
+    {
+      title: "Name",
+      dataIndex: "name",
+    },
+    {
+      title: "Date",
+      dataIndex: "date",
+    },
+  ];
+
+  const data = [
+    {
+      key: "1",
+      name: "Excel 1",
+      date: "05/09/2019",
+    },
+    {
+      key: "2",
+      name: "Excel 2",
+      date: "04/09/2019",
+    },
+    {
+      key: "3",
+      name: "Excel 3",
+      date: "03/09/2019",
+    },
+    {
+      key: "4",
+      name: "Excel 4",
+      date: "02/09/2019",
+    },
+    {
+      key: "5",
+      name: "Excel 5",
+      date: "01/09/2019",
+    },
+  ];
+
+  return <Table columns={columns} dataSource={data} />;
 };
