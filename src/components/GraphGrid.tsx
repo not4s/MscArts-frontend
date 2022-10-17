79aa47cfd8568a7bdaf7810cf646a9f164092415
Suhaib <skm20@ic.ac.uk>
2022-10-17 10:30:48 +0100

    Set up Rows and Columns for graphs

--- src/components/GraphGrid.tsx
+++ src/components/GraphGrid.tsx
@@ -1,21 +1,45 @@
 import { Col, Row } from "antd";
-import React from "react";
+import React, { ReactNode } from "react";
 import Graph from "./Graph";
 
 const GraphGrid = () => {
-  return (
-    <>
+  const list = [
+    <Graph />,
+    <Graph />,
+    <Graph />,
+    <Graph />,
+    <Graph />,
+    <Graph />,
+    <Graph />,
+    <Graph />,
+    <Graph />,
+    <Graph />,
+  ];
+  const rows: JSX.Element[][] = sliceIntoChunks(list, 3);
+  console.log(rows);
+  const nodes = rows.map((row) => {
+    return (
       <Row>
-        <Col span={12}>
-          {" "}
-          <Graph />{" "}
-        </Col>
-        {/* <Col span={6}>col-6</Col> */}
-        {/* <Col span={6}>col-6</Col> */}
-        <Col span={12}>col-6</Col>
+        {" "}
+        {row.map((graph) => {
+          return <Col span={24 / row.length}> {graph} </Col>;
+        })}{" "}
       </Row>
-    </>
-  );
+    );
+  });
+  console.log(nodes);
+
+  return <>{nodes}</>;
 };
 
 export default GraphGrid;
+
+function sliceIntoChunks(arr: any[], len: number) {
+  let chunks = [],
+    i = 0,
+    n = arr.length;
+  while (i < n) {
+    chunks.push(arr.slice(i, (i += len)));
+  }
+  return chunks;
+}
