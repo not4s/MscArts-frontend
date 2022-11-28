0316609999f4d5347b3cfe8d2686717411bc125c
Suhaib <skm20@ic.ac.uk>
2022-11-28 10:51:01 +0000

    moved targets into modal

--- src/components/Settings/TargetForm.jsx
+++ src/components/Settings/TargetForm.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { Button, Form, Select, Input, Progress, Spin } from "antd";
+import {Button, Form, Select, Input, Progress, Spin, Modal} from "antd";
 import { useState } from 'react';
 import { APIService } from '../../services/API';
 import { ApiOutlined } from '@ant-design/icons';
@@ -7,15 +7,22 @@ import { ApiOutlined } from '@ant-design/icons';
 const TargetForm = () => {
   const [targets, setTargets] = useState([])
   const [loaded, setLoaded] = useState(false)
+  const [showModal, setShowModal] = useState(false);
   const programs = ["AIML", "MAI", "MAC", "MCS", "MCSS"]
   const api = new APIService();
 
   const submitNewTarget = (values) => {
+    console.log(values);
     api.postTarget(values)
-      .then(res => {
-        console.log(res);
-      })
-      .catch(err => console.log(err))
+        .then(res => {
+          setShowModal(false);
+          setLoaded(false);
+          console.log(res);
+          api.getTargets()
+              .then(res => { console.log(res.data); setTargets(res.data); setLoaded(true);})
+              .catch(err => {console.log(err); setLoaded(true)})
+        })
+        .catch(err => console.log(err))
   }
 
   const handleRemove = (e, course, year) => {
@@ -36,46 +43,50 @@ const TargetForm = () => {
 
   return (
     <div>
-        You can set targets for each course here. 
-        <Form 
-        name="targets"
-        labelCol={{span: 2}}
-        onFinish={submitNewTarget}
-        >
-          <Form.Item  name="course" style={{width:240}} rules={[{required:true}]}>
-            <Select placeholder="Course"> 
-              {
-                programs.map(program => {return(
-                  <Select.Option value={program}>
-                    {program}
-                  </Select.Option>
-                )})
-              }
-            </Select>
-          </Form.Item>
-          <Form.Item  name="year" style={{width:240}} rules={[{required:true}]}>
-            <Select placeholder="Year">
-              <Select.Option value="2023">22/23</Select.Option>
-              <Select.Option value="2022">21/22</Select.Option>
-              <Select.Option value="2021">20/21</Select.Option>
-              <Select.Option value="2020">19/20</Select.Option>
-              <Select.Option value="2019">18/19</Select.Option>
-            </Select>
-          </Form.Item>
-          <Form.Item name="feeStatus" style={{width:240}} rules={[{required:true}]} >
-            <Select placeholder="Fee Status">
-              <Select.Option value="Overseas">Overseas</Select.Option>
-              <Select.Option value="Home">Home</Select.Option>
-            </Select>
-          </Form.Item>
-          <Form.Item  name="target" style={{width:240}} rules={[{required:true}]}>
-            <Input placeholder="Target"/>
-          </Form.Item>
-       
-          <Form.Item>
-            <Button htmlType='submit'>Submit new target</Button>
-          </Form.Item>
-        </Form>
+       <Modal open={showModal} footer={null}>
+         You can set targets for each course here.
+         <Form
+             name="targets"
+             labelCol={{span: 2}}
+             onFinish={submitNewTarget}
+         >
+           <Form.Item  name="course" style={{width:240}} rules={[{required:true}]}>
+             <Select placeholder="Course">
+               {
+                 programs.map(program => {return(
+                     <Select.Option value={program}>
+                       {program}
+                     </Select.Option>
+                 )})
+               }
+             </Select>
+           </Form.Item>
+           <Form.Item  name="year" style={{width:240}} rules={[{required:true}]}>
+             <Select placeholder="Year">
+               <Select.Option value="2023">22/23</Select.Option>
+               <Select.Option value="2022">21/22</Select.Option>
+               <Select.Option value="2021">20/21</Select.Option>
+               <Select.Option value="2020">19/20</Select.Option>
+               <Select.Option value="2019">18/19</Select.Option>
+             </Select>
+           </Form.Item>
+           <Form.Item name="feeStatus" style={{width:240}} rules={[{required:true}]} >
+             <Select placeholder="Fee Status">
+               <Select.Option value="Overseas">Overseas</Select.Option>
+               <Select.Option value="Home">Home</Select.Option>
+             </Select>
+           </Form.Item>
+           <Form.Item  name="target" style={{width:240}} rules={[{required:true}]}>
+             <Input placeholder="Target"/>
+           </Form.Item>
+
+           <Form.Item>
+             <Button htmlType='submit'>Submit new target</Button>
+           </Form.Item>
+         </Form>
+       </Modal>
+
+      <Button onClick={()=>setShowModal(true)}>Add new target</Button>
       <div>
         <>
           {
